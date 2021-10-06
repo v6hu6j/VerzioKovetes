d6939ddfabd6c1bca2a396ecee439ab1e7c10d93
Liza Lengyel <liza.lengyel2@stud.uni-corvinus.hu>
2021-10-06 19:31:29 +0200

    getcell

--- week04/week04/Form1.cs
+++ week04/week04/Form1.cs
@@ -91,5 +91,21 @@ namespace week04
                 counter++;
             }
         }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
     }
 }
