0f0e8f37c2b68cdcdf34c2fd5148824205d1cdcc
Liza Lengyel <liza.lengyel2@stud.uni-corvinus.hu>
2021-11-11 13:00:36 +0100

    k

---
+++ week05/UserMaintenance/UserMaintenance/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaintenance.Entities
+{
+    public class User
+    {   
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string FullName{ get; set; }
+    }
+}
