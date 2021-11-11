deecee4533dde3f5b5c476aff699856618b9ff01
Liza Lengyel <liza.lengyel2@stud.uni-corvinus.hu>
2021-11-11 12:17:48 +0100

    5

--- gyak7/gyak7/gyak7/Entities/Ball.cs
+++ gyak7/gyak7/gyak7/Entities/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak7.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Windows.Forms;
 
 namespace gyak7.Entities
 {
-    public class Ball : Label
+    public class Ball : Toy
     {
         public Ball()
         {
@@ -23,7 +24,7 @@ namespace gyak7.Entities
             DrawImage(e.Graphics);
         }
 
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
