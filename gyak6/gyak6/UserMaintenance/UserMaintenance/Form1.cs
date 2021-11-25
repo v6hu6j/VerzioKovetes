d6af55b1b61037ae1995ef067b4df4a1dbdee3a5
Péter Ács <peter.acs1@stud.uni-corvinus.hu>
2021-11-25 18:56:35 +0100

    initial

---
+++ gyak6/gyak6/UserMaintenance/UserMaintenance/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UserMaintenance.Entities;
+
+namespace UserMaintenance
+{
+    public partial class Form1 : Form
+    {
+        BindingList<User> users = new BindingList<User>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = Resource1.LastName;
+
+            button1.Text = Resource1.Add;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+
+            var u = new User()
+            {
+                FullName = textBox1.Text,
+
+            };
+            users.Add(u);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var u in users)
+                {
+                    sw.Write(u.FullName);
+                    sw.Write(';');
+                    sw.Write(u.ID);
+                    sw.Write("\n");
+                }
+            }
+        }
+    }
+}
